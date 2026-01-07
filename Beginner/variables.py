a21574b7d3f1fee79a6f6cc5de0a14c2901b9cd9
NandaKishor-mnks <nandakishorsingh81@gmail.com>
2026-01-07 22:33:04 +0530

    Add variables demonstration program in Python

---
+++ Beginner/variables.py
@@ -0,0 +1,9 @@
+# Program to demonstrate variables in Python
+
+name = "Nanda Kishor Singh"
+age = 20
+course = "Python Development Internship"
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
