84064c909caa0a2c7c8e29fc84fe10e2b27d8a40
NandaKishor-mnks <nandakishorsingh81@gmail.com>
2026-01-10 21:50:16 +0530

    Update variables.py

--- Beginner/variables.py
+++ Beginner/variables.py
@@ -1,9 +1,15 @@
 # Program to demonstrate variables in Python
+#1.create a variable named pi and check its type
+pi=22/7
+print("Data type of pi:",type(pi))
 
-name = "Nanda Kishor Singh"
-age = 20
-course = "Python Development Internship"
+#2.Create a variable called "for" and assign it a value 4
+for=4
+#This is an syntaxerror.The keywords in python can't be used as a variable names 
 
-print("Name:", name)
-print("Age:", age)
-print("Course:", course)
+#3.Simple Interest Calculation
+principal_amount=10000
+rate_of_interest=5
+time=3
+SI=(principal_amount*rate_of_interest*time)/100
+print("Simple Interest for 3 years:",SI)
